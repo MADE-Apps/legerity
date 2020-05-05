5312bf74ecb22281ab2ccdd9b74e9381d9a12be0
James Croft <jamz_c@hotmail.co.uk>
2020-05-05 21:15:19 +0100

    Fixed XAML Controls Gallery sample base test class.

--- samples/XamlControlsGallery/BaseTestClass.cs
+++ samples/XamlControlsGallery/BaseTestClass.cs
@@ -20,7 +20,7 @@ namespace XamlControlsGallery
         [TestCleanup]
         public virtual void Cleanup()
         {
-            // AppManager.StopApp();
+            AppManager.StopApp();
         }
     }
 }
