0193517caf8f883a2db9c7617b0e0d10db4114fd
James Croft <jamz_c@hotmail.co.uk>
2020-05-10 16:29:10 +0100

    Added Button Windows element.

---
+++ src/Legerity/Windows/Elements/Core/Button.cs
@@ -0,0 +1,65 @@
+namespace Legerity.Windows.Elements.Core
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using Legerity.Windows.Extensions;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Appium;
+    using OpenQA.Selenium.Appium.Windows;
+
+    /// <summary>
+    /// Defines a <see cref="WindowsElement"/> wrapper for the core UWP Button control.
+    /// </summary>
+    public class Button : WindowsElementWrapper
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Button"/> class.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="WindowsElement"/> reference.
+        /// </param>
+        public Button(WindowsElement element)
+            : base(element)
+        {
+        }
+
+        /// <summary>
+        /// Allows conversion of a <see cref="WindowsElement"/> to the <see cref="Button"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="WindowsElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Button"/>.
+        /// </returns>
+        public static implicit operator Button(WindowsElement element)
+        {
+            return new Button(element);
+        }
+
+        /// <summary>
+        /// Allows conversion of a <see cref="AppiumWebElement"/> to the <see cref="Button"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="AppiumWebElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Button"/>.
+        /// </returns>
+        public static implicit operator Button(AppiumWebElement element)
+        {
+            return new Button(element as WindowsElement);
+        }
+
+        /// <summary>
+        /// Clicks the app bar button.
+        /// </summary>
+        public void Click()
+        {
+            this.Element.Click();
+        }
+    }
+}
