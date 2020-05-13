b1443b8f5f9decfdb08cf1a2c05d44f1c7a4bb6b
James Croft <jamz_c@hotmail.co.uk>
2020-05-13 08:51:15 +0100

    Added element wrapper for the UWP Hub

---
+++ src/Legerity/Windows/Elements/Core/Hub.cs
@@ -0,0 +1,67 @@
+namespace Legerity.Windows.Elements.Core
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+
+    using Legerity.Extensions;
+    using Legerity.Windows.Extensions;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Appium;
+    using OpenQA.Selenium.Appium.Windows;
+    using OpenQA.Selenium.Remote;
+
+    /// <summary>
+    /// Defines a <see cref="WindowsElement"/> wrapper for the core UWP Hub control.
+    /// </summary>
+    public class Hub : WindowsElementWrapper
+    {
+        private readonly By hubSectionItemQuery = By.ClassName("HubSection");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hub"/> class.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="WindowsElement"/> reference.
+        /// </param>
+        public Hub(WindowsElement element)
+            : base(element)
+        {
+        }
+
+        /// <summary>
+        /// Gets the collection of items associated with the hub.
+        /// </summary>
+        public ReadOnlyCollection<AppiumWebElement> Items => this.Element.FindElements(this.hubSectionItemQuery);
+        
+        /// <summary>
+        /// Allows conversion of a <see cref="WindowsElement"/> to the <see cref="Hub"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="WindowsElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Hub"/>.
+        /// </returns>
+        public static implicit operator Hub(WindowsElement element)
+        {
+            return new Hub(element);
+        }
+
+        /// <summary>
+        /// Allows conversion of a <see cref="AppiumWebElement"/> to the <see cref="Hub"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="AppiumWebElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Hub"/>.
+        /// </returns>
+        public static implicit operator Hub(AppiumWebElement element)
+        {
+            return new Hub(element as WindowsElement);
+        }
+    }
+}
