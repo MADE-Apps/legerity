9be2259a5f6bca00c82b830ac8c6245f2bbad709
James Croft <jamz_c@hotmail.co.uk>
2020-05-13 08:54:08 +0100

    Added wrapper for HyperlinkButton

--- src/Legerity/Windows/Elements/Core/HyperlinkButton.cs
+++ src/Legerity/Windows/Elements/Core/HyperlinkButton.cs
@@ -11,9 +11,9 @@ namespace Legerity.Windows.Elements.Core
     using OpenQA.Selenium.Appium.Windows;
 
     /// <summary>
-    /// Defines a <see cref="WindowsElement"/> wrapper for the core UWP Button control.
+    /// Defines a <see cref="WindowsElement"/> wrapper for the core UWP HyperlinkButton control.
     /// </summary>
-    public class Button : WindowsElementWrapper
+    public class HyperlinkButton : WindowsElementWrapper
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Button"/> class.
@@ -21,41 +21,41 @@ namespace Legerity.Windows.Elements.Core
         /// <param name="element">
         /// The <see cref="WindowsElement"/> reference.
         /// </param>
-        public Button(WindowsElement element)
+        public HyperlinkButton(WindowsElement element)
             : base(element)
         {
         }
 
         /// <summary>
-        /// Allows conversion of a <see cref="WindowsElement"/> to the <see cref="Button"/> without direct casting.
+        /// Allows conversion of a <see cref="WindowsElement"/> to the <see cref="HyperlinkButton"/> without direct casting.
         /// </summary>
         /// <param name="element">
         /// The <see cref="WindowsElement"/>.
         /// </param>
         /// <returns>
-        /// The <see cref="Button"/>.
+        /// The <see cref="HyperlinkButton"/>.
         /// </returns>
-        public static implicit operator Button(WindowsElement element)
+        public static implicit operator HyperlinkButton(WindowsElement element)
         {
-            return new Button(element);
+            return new HyperlinkButton(element);
         }
 
         /// <summary>
-        /// Allows conversion of a <see cref="AppiumWebElement"/> to the <see cref="Button"/> without direct casting.
+        /// Allows conversion of a <see cref="AppiumWebElement"/> to the <see cref="HyperlinkButton"/> without direct casting.
         /// </summary>
         /// <param name="element">
         /// The <see cref="AppiumWebElement"/>.
         /// </param>
         /// <returns>
-        /// The <see cref="Button"/>.
+        /// The <see cref="HyperlinkButton"/>.
         /// </returns>
-        public static implicit operator Button(AppiumWebElement element)
+        public static implicit operator HyperlinkButton(AppiumWebElement element)
         {
-            return new Button(element as WindowsElement);
+            return new HyperlinkButton(element as WindowsElement);
         }
 
         /// <summary>
-        /// Clicks the button.
+        /// Clicks the hyperlink button.
         /// </summary>
         public void Click()
         {
