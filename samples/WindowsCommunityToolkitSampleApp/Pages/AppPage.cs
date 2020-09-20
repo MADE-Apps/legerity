f767cef6515a101d920358fb5aec847d1dbed6f9
James Croft <jamz_c@hotmail.co.uk>
2020-09-20 11:57:41 +0100

    Added sample tests for WCT sample app for RadialGauge

---
+++ samples/WindowsCommunityToolkitSampleApp/Pages/AppPage.cs
@@ -0,0 +1,51 @@
+namespace WindowsCommunityToolkitSampleApp.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Legerity.Pages;
+    using Legerity.Windows.Elements.Core;
+    using Legerity.Windows.Elements.WinUI;
+    using Legerity.Windows.Extensions;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Appium;
+
+    public class AppPage : BasePage
+    {
+        private readonly By sampleSearchBoxQuery = ByExtensions.AutomationId("SearchBox");
+
+        /// <summary>
+        /// Gets the UI component associated with the sample picker.
+        /// </summary>
+        public GridView SamplePicker => this.WindowsApp.FindElement(ByExtensions.AutomationId("SamplePickerGridView"));
+
+        /// <summary>
+        /// Gets a given trait of the page to verify that the page is in view.
+        /// </summary>
+        protected override By Trait => ByExtensions.AutomationId("NavView");
+
+        /// <summary>
+        /// Selects a sample from the available options with the given name.
+        /// </summary>
+        /// <typeparam name="TPage">The type of page to return.</typeparam>
+        /// <param name="name">The name of the sample to click.</param>
+        /// <returns>The <see cref="TPage"/> instance.</returns>
+        public TPage SelectSample<TPage>(string name) where TPage : BasePage
+        {
+            this.SearchForSample(name);
+
+            AppiumWebElement item = this.SamplePicker.Items.FirstOrDefault(i =>
+                i.FindElement(By.XPath($".//*[@ClassName='TextBlock'][@Name='{name}']")) != null);
+            item.Click();
+
+            return Activator.CreateInstance<TPage>();
+        }
+
+        private void SearchForSample(string sampleName)
+        {
+            NavigationView navigationView = this.WindowsApp.FindElement(this.Trait);
+            AutoSuggestBox controlsSearchBox = navigationView.Element.FindElement(this.sampleSearchBoxQuery);
+            controlsSearchBox.SetText(sampleName);
+        }
+    }
+}
---
+++ samples/WindowsCommunityToolkitSampleApp/Pages/RadialGaugePage.cs
@@ -0,0 +1,38 @@
+namespace WindowsCommunityToolkitSampleApp.Pages
+{
+    using Legerity.Windows.Elements.WCT;
+    using Legerity.Windows.Extensions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+
+    /// <summary>
+    /// Defines the RadialGauge page of the Windows Community Toolkit sample application.
+    /// </summary>
+    public class RadialGaugePage : AppPage
+    {
+        private readonly By radialGauge;
+
+        public RadialGaugePage()
+        {
+            this.radialGauge = ByExtensions.AutomationId("RadialGauge");
+        }
+
+        /// <summary>
+        /// Gets a given trait of the page to verify that the page is in view.
+        /// </summary>
+        protected override By Trait => By.XPath(".//*[@ClassName='TextBlock'][@Name='RadialGauge']");
+
+        public RadialGaugePage SetRadialGaugeValue(double value)
+        {
+            RadialGauge gauge = this.WindowsApp.FindElement(this.radialGauge);
+            gauge.SetValue(value);
+            return this;
+        }
+
+        public void VerifyRadialGaugeValue(double value)
+        {
+            RadialGauge gauge = this.WindowsApp.FindElement(this.radialGauge);
+            Assert.AreEqual(value, gauge.Value);
+        }
+    }
+}
