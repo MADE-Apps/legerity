191fb79c5504f17896a8ec46b0d8da01a3b58f23
James Croft <jamz_c@hotmail.co.uk>
2020-09-26 20:41:37 +0100

    Added tests for Expander based on the changes to the WCT. Also included IDs for both store and debug sample apps.

--- samples/WindowsCommunityToolkitSampleApp/BaseTestClass.cs
+++ samples/WindowsCommunityToolkitSampleApp/BaseTestClass.cs
@@ -6,11 +6,17 @@ namespace WindowsCommunityToolkitSampleApp
 
     public abstract class BaseTestClass
     {
+        // The sample application ID if the WCT sample app is built through Visual Studio.
+        private const string DebugSampleApp = "52b9212c-97a9-4639-9426-3e1ea9c1569e_vpw621za47p9m!App";
+
+        // The sample application ID if the WCT sample app is installed from the Windows Store.
+        private const string StoreSampleApp = "Microsoft.UWPCommunityToolkitSampleApp_8wekyb3d8bbwe!App";
+
         [TestInitialize]
         public virtual void Initialize()
         {
             AppManager.StartApp(
-                new WindowsAppManagerOptions("Microsoft.UWPCommunityToolkitSampleApp_8wekyb3d8bbwe!App")
+                new WindowsAppManagerOptions(DebugSampleApp)
                 {
                     DriverUri = "http://127.0.0.1:4723"
                 });
