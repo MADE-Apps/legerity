c59dfddfc77816a0ef86f35bd6fe92fdc84fcc44
James Croft <jamz_c@hotmail.co.uk>
2020-10-09 21:50:35 +0100

    Added MenuBar element and additional required element wrappers

--- samples/XamlControlsGallery/BaseTestClass.cs
+++ samples/XamlControlsGallery/BaseTestClass.cs
@@ -7,11 +7,17 @@ namespace XamlControlsGallery
 
     public abstract class BaseTestClass
     {
+        // The sample application ID if the XamlControlsGallery sample app is built through Visual Studio.
+        private const string DebugSampleApp = "Microsoft.XAMLControlsGallery.Debug_8wekyb3d8bbwe!App";
+
+        // The sample application ID if the XamlControlsGallery sample app is installed from the Windows Store.
+        private const string StoreSampleApp = "Microsoft.XAMLControlsGallery_8wekyb3d8bbwe!App";
+
         [TestInitialize]
         public virtual void Initialize()
         {
             AppManager.StartApp(
-                new WindowsAppManagerOptions("Microsoft.XAMLControlsGallery_8wekyb3d8bbwe!App")
+                new WindowsAppManagerOptions(DebugSampleApp)
                 {
                     DriverUri = "http://127.0.0.1:4723"
                 });
