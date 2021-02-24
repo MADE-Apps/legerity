99851e83ce1377e3618ca38f92e83ce2264605ff
James Croft <jamz_c@hotmail.co.uk>
2021-02-24 08:44:04 +0000

    #34 - Added Android RadioButton element

---
+++ src/Legerity/Android/Elements/Core/RadioButton.cs
@@ -0,0 +1,59 @@
+namespace Legerity.Android.Elements.Core
+{
+    using System;
+    using OpenQA.Selenium.Appium;
+    using OpenQA.Selenium.Appium.Android;
+
+    /// <summary>
+    /// Defines a <see cref="AndroidElement"/> wrapper for the core Android RadioButton control.
+    /// </summary>
+    public class RadioButton : Button
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadioButton"/> class.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="AndroidElement"/> reference.
+        /// </param>
+        public RadioButton(AndroidElement element)
+            : base(element)
+        {
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the radio button is selected.
+        /// </summary>
+        public bool IsSelected =>
+            this.Element.GetAttribute("Checked").Equals(
+                "True",
+                StringComparison.CurrentCultureIgnoreCase);
+
+        /// <summary>
+        /// Allows conversion of a <see cref="AndroidElement"/> to the <see cref="RadioButton"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="AndroidElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="RadioButton"/>.
+        /// </returns>
+        public static implicit operator RadioButton(AndroidElement element)
+        {
+            return new RadioButton(element);
+        }
+
+        /// <summary>
+        /// Allows conversion of a <see cref="AppiumWebElement"/> to the <see cref="RadioButton"/> without direct casting.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="AppiumWebElement"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="RadioButton"/>.
+        /// </returns>
+        public static implicit operator RadioButton(AppiumWebElement element)
+        {
+            return new RadioButton(element as AndroidElement);
+        }
+    }
+}
