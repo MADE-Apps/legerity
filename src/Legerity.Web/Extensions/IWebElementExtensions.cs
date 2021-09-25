da1a43a10dcc576191d4d35913a30fbecc3d3310
James Croft <jamz_c@hotmail.co.uk>
2021-09-25 14:42:05 +0100

    Added web element extension to determine if an element has a specific class name.

--- src/Legerity.Web/Extensions/IWebElementExtensions.cs
+++ src/Legerity.Web/Extensions/IWebElementExtensions.cs
@@ -16,5 +16,16 @@ namespace Legerity.Web.Extensions
         {
             return element.GetAttribute("innerHTML");
         }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="IWebElement"/> has the specified <paramref name="className">class</paramref>.
+        /// </summary>
+        /// <param name="element">The <see cref="IWebElement"/> to check.</param>
+        /// <param name="className">The name of the class the element should have.</param>
+        /// <returns>True if the element has the class; otherwise, false.</returns>
+        public static bool HasClass(this IWebElement element, string className)
+        {
+            return element.GetAttribute("class").Contains(className);
+        }
     }
 }
