1a7218926c41719f82d675fe11857a8b11bc4c4d
James Croft <jamz_c@hotmail.co.uk>
2022-03-18 09:23:25 +0000

    Added per platform element wrapper extensions for WaitUntil

---
+++ src/Legerity.Android/Extensions/AndroidElementWrapperExtensions.cs
@@ -0,0 +1,36 @@
+namespace Legerity.Android.Extensions
+{
+    using System;
+    using Legerity.Android.Elements;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+
+    /// <summary>
+    /// Defines a collection of extensions for <see cref="AndroidElementWrapper"/> objects.
+    /// </summary>
+    public static class AndroidElementWrapperExtensions
+    {
+        /// <summary>
+        /// Waits until a specified element condition is met, with an optional timeout.
+        /// </summary>
+        /// <param name="element">The element to wait on.</param>
+        /// <param name="condition">The condition of the element to wait on.</param>
+        /// <param name="timeout">The optional timeout wait on the condition being true.</param>
+        /// <typeparam name="TElementWrapper">The type of <see cref="AndroidElementWrapper"/>.</typeparam>
+        public static void WaitUntil<TElementWrapper>(this TElementWrapper element, Func<TElementWrapper, bool> condition, TimeSpan? timeout = default)
+            where TElementWrapper : AndroidElementWrapper
+        {
+            new WebDriverWait(AppManager.App, timeout ?? TimeSpan.Zero).Until(driver =>
+            {
+                try
+                {
+                    return condition(element);
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+            });
+        }
+    }
+}
