c53b8fa97cb5d3b6baeddbd818c9ecf9231880c2
James Croft <jamz_c@hotmail.co.uk>
2023-01-02 19:18:35 +0000

    ✅ Added web Option tests

---
+++ tests/Legerity.Web.Tests/Pages/OptionPage.cs
@@ -0,0 +1,30 @@
+namespace Legerity.Web.Tests.Pages;
+
+using System.Collections.Generic;
+using System.Linq;
+using Elements.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+internal class OptionPage : W3SchoolsBasePage
+{
+    private readonly By carOptionLocator = By.TagName("option");
+
+    public OptionPage(RemoteWebDriver app) : base(app)
+    {
+    }
+
+    public IEnumerable<Option> CarOptions => this.FindElements(this.carOptionLocator).Select(e => new Option(e));
+
+    public OptionPage SelectCarOptionByDisplayValue(string option)
+    {
+        this.CarOptions.FirstOrDefault(o => o.DisplayValue == option).Select();
+        return this;
+    }
+
+    public OptionPage SelectCarOptionByValue(string option)
+    {
+        this.CarOptions.FirstOrDefault(o => o.Value == option).Select();
+        return this;
+    }
+}
