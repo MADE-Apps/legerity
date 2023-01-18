26a00a8ea319978f341394396cc79ecc913e71e4
James Croft <jamz_c@hotmail.co.uk>
2023-01-18 21:18:56 +0000

    Add Windows ComboBox tests

---
+++ tests/Legerity.Windows.Tests/Tests/ComboBoxTests.cs
@@ -0,0 +1,42 @@
+namespace Legerity.Windows.Tests.Tests;
+
+using OpenQA.Selenium.Remote;
+using Pages;
+using Shouldly;
+
+[TestFixtureSource(nameof(PlatformOptions))]
+internal class ComboBoxTests : BaseTestClass
+{
+    public ComboBoxTests(AppManagerOptions options)
+        : base(options)
+    {
+    }
+
+    [Test]
+    public void ShouldSelectItemByName()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        ComboBoxPage comboBoxPage = new HomePage(app).NavigateTo<ComboBoxPage>("ComboBox");
+
+        // Act
+        comboBoxPage.SelectColorByName("Red");
+
+        // Assert
+        comboBoxPage.ColorComboBox.SelectedItem.ShouldBe("Red");
+    }
+
+    [Test]
+    public void ShouldSelectItemByPartialName()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        ComboBoxPage comboBoxPage = new HomePage(app).NavigateTo<ComboBoxPage>("ComboBox");
+
+        // Act
+        comboBoxPage.SelectColorByPartialName("Yell");
+
+        // Assert
+        comboBoxPage.ColorComboBox.SelectedItem.ShouldBe("Yellow");
+    }
+}
