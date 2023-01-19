a617748253b5c75042c00b14774bcf0fad9ed3af
James Croft <jamz_c@hotmail.co.uk>
2023-01-19 20:51:48 +0000

    Add Windows CalendarView tests

---
+++ tests/Legerity.Windows.Tests/Pages/CalendarViewPage.cs
@@ -0,0 +1,22 @@
+namespace Legerity.Windows.Tests.Pages;
+
+using System;
+using Legerity.Pages;
+using Legerity.Windows.Elements.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+internal class CalendarViewPage : BaseNavigationPage
+{
+    public CalendarViewPage(RemoteWebDriver app) : base(app)
+    {
+    }
+
+    public CalendarView CalendarView => this.FindElement(By.ClassName(nameof(this.CalendarView)));
+    
+    public CalendarViewPage SetCalendarViewDate(DateTime date)
+    {
+        this.CalendarView.SetDate(date);
+        return this;
+    }
+}
---
+++ tests/Legerity.Windows.Tests/Tests/CalendarViewTests.cs
@@ -0,0 +1,48 @@
+namespace Legerity.Windows.Tests.Tests;
+
+using Legerity.Windows.Tests.Pages;
+using OpenQA.Selenium.Remote;
+using Shouldly;
+using System.Globalization;
+using System;
+
+[TestFixtureSource(nameof(PlatformOptions))]
+internal class CalendarViewTests : BaseTestClass
+{
+    public CalendarViewTests(AppManagerOptions options)
+        : base(options)
+    {
+    }
+
+    [TestCase("12/12/2022")]
+    [TestCase("01/01/2023")]
+    [TestCase("02/02/2023")]
+    public void ShouldSelectDate(string dateString)
+    {
+        // Arrange
+        var expectedDate = DateTime.Parse(dateString, CultureInfo.InvariantCulture);
+        RemoteWebDriver app = this.StartApp();
+        CalendarViewPage calendarViewPage = new HomePage(app).NavigateTo<CalendarViewPage>("CalendarView");
+
+        // Act
+        calendarViewPage.SetCalendarViewDate(expectedDate);
+
+        // Assert
+        calendarViewPage.CalendarView.SelectedDate.ShouldBe(expectedDate);
+    }
+
+    [Test]
+    public void ShouldSelectDateWithFormat()
+    {
+        // Arrange
+        DateTime expectedDate = DateTime.Now.AddDays(1);
+        RemoteWebDriver app = this.StartApp();
+        CalendarViewPage calendarViewPage = new HomePage(app).NavigateTo<CalendarViewPage>("CalendarView");
+
+        // Act
+        calendarViewPage.SetCalendarViewDate(expectedDate);
+
+        // Assert
+        calendarViewPage.CalendarView.Value.ShouldBe(expectedDate.ToString("MMMM dd, yyyy", CultureInfo.CurrentCulture));
+    }
+}
