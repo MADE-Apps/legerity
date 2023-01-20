7bb05414dd298378519157dfb4e8954e28442a89
James Croft <jamz_c@hotmail.co.uk>
2023-01-20 05:23:19 +0000

    Add Windows CheckBox tests

---
+++ tests/Legerity.Windows.Tests/Pages/CheckBoxPage.cs
@@ -0,0 +1,38 @@
+namespace Legerity.Windows.Tests.Pages;
+
+using Elements.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+internal class CheckBoxPage : BaseNavigationPage
+{
+    public CheckBoxPage(RemoteWebDriver app) : base(app)
+    {
+    }
+
+    public CheckBox TwoStateCheckBox => this.FindElement(By.Name("Two-state CheckBox"));
+
+    public CheckBox ThreeStateCheckBox => this.FindElement(By.Name("Three-state CheckBox"));
+
+    public CheckBoxPage CheckOnTwoStateCheckBox()
+    {
+        this.TwoStateCheckBox.CheckOn();
+        return this;
+    }
+
+    public CheckBoxPage CheckOffTwoStateCheckBox()
+    {
+        this.TwoStateCheckBox.CheckOff();
+        return this;
+    }
+
+    public CheckBoxPage CheckIndeterminateThreeStateCheckBox()
+    {
+        while (!this.ThreeStateCheckBox.IsIndeterminate)
+        {
+            this.ThreeStateCheckBox.Click();
+        }
+
+        return this;
+    }
+}
