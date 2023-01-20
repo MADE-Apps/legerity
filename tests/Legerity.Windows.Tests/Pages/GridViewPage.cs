6cd4bceb46b21c77034c208d02188aa8ca557355
James Croft <jamz_c@hotmail.co.uk>
2023-01-20 07:37:21 +0000

    Add Windows GridView tests

---
+++ tests/Legerity.Windows.Tests/Pages/GridViewPage.cs
@@ -0,0 +1,32 @@
+namespace Legerity.Windows.Tests.Pages;
+
+using System.Drawing;
+using Elements.Core;
+using OpenQA.Selenium.Remote;
+
+internal class GridViewPage : BaseNavigationPage
+{
+    public GridViewPage(RemoteWebDriver app) : base(app)
+    {
+    }
+
+    public GridView BasicGridView => this.FindElement(WindowsByExtras.AutomationId("BasicGridView"));
+
+    public GridViewPage ClickBasicGridViewItem(string name)
+    {
+        this.BasicGridView.ClickItem(name);
+        return this;
+    }
+
+    public GridViewPage ClickBasicGridViewItemByPartialName(string name)
+    {
+        this.BasicGridView.ClickItemByPartialName(name);
+        return this;
+    }
+
+    public GridViewPage ClickBasicGridViewItemByIndex(int index)
+    {
+        this.BasicGridView.ClickItemByIndex(index);
+        return this;
+    }
+}
