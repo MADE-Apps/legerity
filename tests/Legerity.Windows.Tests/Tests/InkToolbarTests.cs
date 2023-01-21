14b23cfb0970c8d93952188f3869e74d0bd2d8a7
James Croft <jamz_c@hotmail.co.uk>
2023-01-21 15:31:17 +0000

    Added Windows InkToolbar tests

---
+++ tests/Legerity.Windows.Tests/Tests/InkToolbarTests.cs
@@ -0,0 +1,98 @@
+namespace Legerity.Windows.Tests.Tests;
+
+using OpenQA.Selenium.Remote;
+using Pages;
+using Shouldly;
+
+[TestFixtureSource(nameof(PlatformOptions))]
+internal class InkToolbarTests : BaseTestClass
+{
+    public InkToolbarTests(AppManagerOptions options)
+        : base(options)
+    {
+    }
+
+    [Test]
+    public void ShouldSetBallpointPenColor()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectBallpointPenColor("Red");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedBallpointPenColor.ShouldBe("Red");
+    }
+
+    [Test]
+    public void ShouldSetPencilColor()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectPencilColor("Red");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedPencilColor.ShouldBe("Red");
+    }
+
+    [Test]
+    public void ShouldSetHighlighterColor()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectHighlighterColor("Pink");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedHighlighterColor.ShouldBe("Pink");
+    }
+
+    [Test]
+    public void ShouldSetBallpointPenColorByPartialName()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectBallpointPenPartialColor("Yell");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedBallpointPenColor.ShouldBe("Yellow");
+    }
+
+    [Test]
+    public void ShouldSetPencilColorByPartialName()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectPencilPartialColor("Yell");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedPencilColor.ShouldBe("Yellow");
+    }
+
+    [Test]
+    public void ShouldSetHighlighterColorByPartialName()
+    {
+        // Arrange
+        RemoteWebDriver app = this.StartApp();
+        InkToolbarPage inkToolbarPage = new HomePage(app).NavigateTo<InkToolbarPage>("InkToolbar");
+
+        // Act
+        inkToolbarPage.SelectHighlighterPartialColor("Yell");
+
+        // Assert
+        inkToolbarPage.InkToolbar.SelectedHighlighterColor.ShouldBe("Yellow");
+    }
+}
