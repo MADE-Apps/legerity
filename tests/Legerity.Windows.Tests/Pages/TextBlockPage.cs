4956349ccb359fbc5fb574f7fe57e2d3dcdb53b6
James Croft <jamz_c@hotmail.co.uk>
2023-01-22 16:00:39 +0000

    Add Windows TextBlock tests

---
+++ tests/Legerity.Windows.Tests/Pages/TextBlockPage.cs
@@ -0,0 +1,14 @@
+namespace Legerity.Windows.Tests.Pages;
+
+using Elements.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+internal class TextBlockPage : BaseNavigationPage
+{
+    public TextBlockPage(RemoteWebDriver app) : base(app)
+    {
+    }
+
+    public TextBlock TextBlock => this.FindElement(By.Name("I am a TextBlock."));
+}
