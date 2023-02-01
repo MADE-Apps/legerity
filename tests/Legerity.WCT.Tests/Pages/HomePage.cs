d1d36d4d511c87d3e19ab7c6ddac2ce67e6eb29b
James Croft <jamz_c@hotmail.co.uk>
2023-02-01 19:59:42 +0000

    Add Community Toolkit test project

---
+++ tests/Legerity.WCT.Tests/Pages/HomePage.cs
@@ -0,0 +1,10 @@
+namespace Legerity.WCT.Tests.Pages;
+
+using OpenQA.Selenium.Remote;
+
+internal class HomePage : BaseNavigationPage
+{
+    public HomePage(RemoteWebDriver app) : base(app)
+    {
+    }
+}
